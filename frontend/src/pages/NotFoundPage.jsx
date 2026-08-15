8c38dc7f606a997832494d7df8203bd0fcfc3350
chihskin-afk <hard.work.ath@gmail.com>
2026-08-15 15:12:51 +0300

    feat(frontend): add reset styles & colors

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,14 +1,9 @@
-import RouterLink from "../components/RouterLink/RouterLink"
-
 const NotFoundPage = () => {
     return (
         <div>
             Page not found
-            <RouterLink 
-                to="/"
-            >
-                <h1>На главную</h1>
-            </RouterLink>
+            <button>sdf</button>
+            <a href="">sdf</a>
         </div>
     )
 }
